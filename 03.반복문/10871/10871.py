a01edaec4e07c75a17c0863dbe3e9675064fab50
Hwang MyungHa <92145054+Alexis8588@users.noreply.github.com>
2022-04-17 15:31:38 +0900

    Update 10871.py

--- 03.반복문/10871/10871.py
+++ 03.반복문/10871/10871.py
@@ -10,7 +10,7 @@
 # X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
 
 N, X = map(int, input().split())
-
-A = []
-for i in range(1, N+1):
-    
+A = list(map(int, input().split()))
+for i in range(N):
+    if A[i] < X:
+        print(A[i], end=" ")
