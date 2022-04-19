c56684d59dad722b75805a51ea98b8a9defe3f8c
Hwang MyungHa <92145054+Alexis8588@users.noreply.github.com>
2022-04-19 17:35:13 +0900

    Finish Repeat
    finish 1110

--- 03.반복문/10951/10951.py
+++ 03.반복문/10951/10951.py
@@ -1,4 +1,3 @@
-# 문제
 # 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
 # 입력
@@ -6,16 +5,20 @@
 
 # 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
-# 입력의 마지막에는 0 두 개가 들어온다.
-
 # 출력
 # 각 테스트 케이스마다 A+B를 출력한다.
 
+while True:
+    try:
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        break
+    
 run = True
 while (run):
-    A, B = map(int, input().split())
-    C = A + B
-    if (A == 0 and B == 0):
+    try:
+        A, B = map(int, input().split())
+        print(A + B)
+    except:
         run = False
-    else:
-        print(C)
---
+++ 04.1차원배열/10818/10818.py
@@ -0,0 +1,25 @@
+# 문제
+# N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+# 출력
+# 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+import sys
+
+N = int(input())
+arr = [N]
+
+for i in range(N):
+    arr[i] = map(int, sys.stdin.readline())
+
+max = arr[0]
+min = arr[0]
+for j in range(len(arr)):
+    if (arr[j] > max):
+        max = arr[j]
+    if (arr[j] < min):
+        min = arr[j]
+print(f'{min} {max}')
